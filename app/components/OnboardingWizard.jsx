431e44adca79675c43c83a0f4a08e0f14f4d40b9
Richard <richard@stromer.org>
2025-07-11 13:21:09 +0200

    add skip to activation step link

--- app/components/OnboardingWizard.jsx
+++ app/components/OnboardingWizard.jsx
@@ -19,6 +19,7 @@ import {
   Grid,
   IconButton,
   InputLabel,
+  Link,
   MenuItem,
   Paper,
   Select,
@@ -432,6 +433,25 @@ const OnboardingWizard = ({ open, onClose, onComplete }) => {
             {loading ? 'Processing...' : 'Get $5 Credits for free'}
           </Button>
         </Paper>
+
+        {/* Activation key option */}
+        <Typography
+          variant="caption"
+          color="text.secondary"
+          align="center"
+          sx={{ mt: 1, display: 'block', mb: 1 }}
+        >
+          Already have an activation key?{' '}
+          <Link
+            href="#"
+            onClick={e => {
+              e.preventDefault();
+              setSignupCompleted(true);
+            }}
+          >
+            Enter it here
+          </Link>
+        </Typography>
 
         {/* Divider */}
         <Divider sx={{ my: 3 }}>
