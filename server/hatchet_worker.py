d8726da64b3e87d3f31a924fbc4994e0222c967e
Sebastian Schuon <schuon@users.noreply.github.com>
2025-08-06 15:45:59 +0200

    Finally registered a worker

--- server/hatchet_worker.py
+++ server/hatchet_worker.py
@@ -5,7 +5,11 @@ Hatchet worker for registering and processing tasks.
 import logging
 
 # Import the tasks to register them
-from server.utils.hatchet_tasks import hatchet
+from server.utils.hatchet_tasks import simple
+from server.utils.hatchet_client import get_hatchet_client
+
+# Get the shared Hatchet client instance
+hatchet = get_hatchet_client()
 
 logger = logging.getLogger(__name__)
 
@@ -19,13 +23,14 @@ def main():
 
     # The tasks are automatically registered when imported
     # The @hatchet.task decorator registers them with the Hatchet instance
+    # Import the simple task to ensure it's registered
 
     logger.info('Tasks registered successfully')
     logger.info('Starting worker to process tasks...')
 
     # Start the Hatchet worker
     # This will register the tasks and start processing them
-    hatchet.worker().start()
+    hatchet.worker('test-worker', workflows=[simple]).start()
 
 
 if __name__ == '__main__':
