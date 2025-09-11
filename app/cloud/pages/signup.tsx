cd700c1f6be94e2b30222f4e8e6fddfc511fdf52
ThiloReintjes <71219244+ThiloReintjes@users.noreply.github.com>
2025-09-11 12:00:21 +0200

    feat: customize sign up flow

--- app/cloud/pages/signup.tsx
+++ app/cloud/pages/signup.tsx
@@ -1,4 +1,5 @@
-import { SignUp as ClerkSignUp } from '@clerk/clerk-react';
+import * as Clerk from '@clerk/elements/common';
+import * as SignUp from '@clerk/elements/sign-up';
 import { Box, Container, Stack, Typography } from '@mui/material';
 
 export default function SignupPage() {
@@ -30,7 +31,43 @@ export default function SignupPage() {
         >
           {/* Left side - Clerk form */}
           <Box>
-            <ClerkSignUp />
+            <SignUp.Root>
+              <SignUp.Step name="start">
+                <Clerk.Connection name="google">Sign up with Google</Clerk.Connection>
+
+                <Clerk.Field name="emailAddress">
+                  <Clerk.Label>Email</Clerk.Label>
+                  <Clerk.Input />
+                  <Clerk.FieldError />
+                </Clerk.Field>
+
+                <Clerk.Field name="password">
+                  <Clerk.Label>Password</Clerk.Label>
+                  <Clerk.Input />
+                  <Clerk.FieldError />
+                </Clerk.Field>
+
+                <Clerk.Field name="unsafeMetadata.softwareToAutomate">
+                  <Clerk.Label>What software do you want to automate?</Clerk.Label>
+                  <Clerk.Input />
+                  <Clerk.FieldError />
+                </Clerk.Field>
+
+                <SignUp.Action submit>Sign up</SignUp.Action>
+              </SignUp.Step>
+
+              <SignUp.Step name="verifications">
+                <SignUp.Strategy name="email_code">
+                  <Clerk.Field name="code">
+                    <Clerk.Label>Email Code</Clerk.Label>
+                    <Clerk.Input />
+                    <Clerk.FieldError />
+                  </Clerk.Field>
+
+                  <SignUp.Action submit>Verify</SignUp.Action>
+                </SignUp.Strategy>
+              </SignUp.Step>
+            </SignUp.Root>
           </Box>
 
           {/* Right side - Marketing text */}
